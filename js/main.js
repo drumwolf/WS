94e88be4401df066442acc22fa13b9a419d7ea06
John Lee <drumwolf@gmail.com>
2019-12-31 13:19:52 -0800

    first draft of product thumbnails

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
+function editProductData(product) {
+  // prepare initial variables
+  const parser = new DOMParser;
+  const priceRange = product.priceRange;
+  const price = (priceRange && priceRange.selling && priceRange.selling.high && !isNaN(priceRange.selling.high) ) ? priceRange.selling.high : 0;
+  // edit 'product' JSON object
+  product.name = parser.parseFromString(product.name,'text/html').body.textContent;
+  product.price = '$' + price.toFixed(2);
+}
 function buildPage(data) {
-  console.log(data)
+  console.log(data.groups);
+  // parse individual data properties
+  const productTemplate = document.getElementById('product-template');
+  const renderProduct = Handlebars.compile(productTemplate.innerHTML);
+  let finalHTML = '';
+  // process metadata for each individual group
+  data.groups.forEach( productData => {
+    editProductData(productData);
+    finalHTML += renderProduct(productData);
+  });
+  document.getElementById('main').innerHTML += finalHTML;
 }
 const url = '/js/data.json';
 fetch(url).then( resp => resp.json() ).then( data => buildPage(data) );
