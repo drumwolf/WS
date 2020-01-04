54e4e58b4fe50946c559f71033bdca8554f464b9
John Lee <drumwolf@gmail.com>
2020-01-04 08:00:39 -0800

    refactored JS into class methods

--- js/main.js
+++ js/main.js
@@ -1,25 +1,37 @@
-function editProductData(product) {
-  // prepare initial variables
-  const parser = new DOMParser;
-  const priceRange = product.priceRange;
-  const price = (priceRange && priceRange.selling && priceRange.selling.high && !isNaN(priceRange.selling.high) ) ? priceRange.selling.high : 0;
-  // edit 'product' JSON object
-  product.name = parser.parseFromString(product.name,'text/html').body.textContent;
-  product.price = '$' + price.toFixed(2);
-}
-function buildPage(data) {
-  console.log(data.groups);
-  // parse individual data properties
-  const productTemplate = document.getElementById('product-template');
-  const renderProduct = Handlebars.compile(productTemplate.innerHTML);
-  let finalHTML = '';
-  // process metadata for each individual group
-  data.groups.forEach( productData => {
-    editProductData(productData);
-    finalHTML += renderProduct(productData);
-  });
-  document.getElementById('main').innerHTML += finalHTML;
-}
-const url = '/js/data.json';
-fetch(url).then( resp => resp.json() ).then( data => buildPage(data) );
+class ProductPage {
+  constructor() {
+    // define variables
+    this.products = [];
+    // fetch JSON data
+    const url = '/js/data.json';
+    fetch(url).then( resp => resp.json() ).then( data => this.buildPage(data) );
+  }
 
+  buildPage(data) {
+    // store product data
+    this.products = data.groups;
+    // parse individual data properties
+    const productTemplate = document.getElementById('product-template');
+    const renderProduct = Handlebars.compile(productTemplate.innerHTML);
+    let finalHTML = '';
+    // process metadata for each individual group
+    this.products.forEach( productData => {
+      this.editProductData(productData);
+      finalHTML += renderProduct(productData);
+    });
+    document.getElementById('main').innerHTML += finalHTML;
+  }
+
+  editProductData(product) {
+    // prepare initial variables
+    const parser = new DOMParser;
+    const priceRange = product.priceRange;
+    const price = (priceRange && priceRange.selling && priceRange.selling.high && !isNaN(priceRange.selling.high) ) ? priceRange.selling.high : 0;
+    // edit 'product' JSON object
+    product.name = parser.parseFromString(product.name,'text/html').body.textContent;
+    product.price = '$' + price.toFixed(2);
+  }
+
+}
+
+new ProductPage();
